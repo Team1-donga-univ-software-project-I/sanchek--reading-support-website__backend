b5df85a69570d05744ce0ce4b5efd053e54dcd1f
RyuJeongSang <42318201+RyuJeongSang@users.noreply.github.com>
2022-10-11 11:37:58 +0900

    feature: User CRUD

---
+++ src/users/dtos/user-profile.dto.ts
@@ -0,0 +1,15 @@
+import { Field, InputType, ObjectType } from "@nestjs/graphql";
+import { CoreOutput } from "src/common/dtos/output.dto";
+import { User } from "../entities/user.entity";
+
+@InputType()
+export class UserProfileInput {
+  @Field((type) => Number)
+  userId: number;
+}
+
+@ObjectType()
+export class UserProfileOutput extends CoreOutput {
+  @Field((type) => User, { nullable: true })
+  user?: User;
+}
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,8 +1,14 @@
-import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { UseGuards } from "@nestjs/common";
+import { Args, Context, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { AuthUser } from "src/auth/auth-user.decorator";
+import { AuthGuard } from "src/auth/auth.guard";
 import {
   CreateAccountInput,
   CreateAccountOutput,
 } from "./dtos/create-account.dto";
+import { EditProfileInput, EditProfileOutput } from "./dtos/edit-profile.dto";
+import { LoginInput, LoginOutput } from "./dtos/login.dto";
+import { UserProfileInput, UserProfileOutput } from "./dtos/user-profile.dto";
 import { User } from "./entities/user.entity";
 import { UserService } from "./users.service";
 
@@ -10,11 +16,79 @@ import { UserService } from "./users.service";
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
-  @Query((returns) => Boolean)
-  users() {
-    return true;
+  @Mutation((returns) => CreateAccountOutput)
+  async createAccount(
+    @Args("input") createAccountInput: CreateAccountInput
+  ): Promise<CreateAccountOutput> {
+    try {
+      const { ok, error } = await this.userService.createAccount(
+        createAccountInput
+      );
+      return {
+        ok,
+        error,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
   }
 
-  @Mutation((returns) => CreateAccountOutput)
-  createAccount(@Args("input") createAccountInput: CreateAccountInput) {}
+  @Mutation((returns) => LoginOutput)
+  async login(@Args("input") loginInput: LoginInput): Promise<LoginOutput> {
+    try {
+      const { ok, error, token } = await this.userService.login(loginInput);
+      return { ok, error, token };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
+
+  @Query((returns) => User)
+  @UseGuards(AuthGuard)
+  me(@AuthUser() authUser: User) {
+    return authUser;
+  }
+
+  @Query((returns) => UserProfileOutput)
+  @UseGuards(AuthGuard)
+  async userProfile(
+    @Args("input") userProfileInput: UserProfileInput
+  ): Promise<UserProfileOutput> {
+    try {
+      const { ok, error, user } = await this.userService.userProfile(
+        userProfileInput
+      );
+      return { ok, error, user };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
+
+  @Mutation((returns) => EditProfileOutput)
+  @UseGuards(AuthGuard)
+  async editProfile(
+    @AuthUser() authUser: User,
+    @Args("input") editProfileInput: EditProfileInput
+  ): Promise<EditProfileOutput> {
+    try {
+      await this.userService.editProfile(authUser.id, editProfileInput);
+      return {
+        ok: true,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
 }
